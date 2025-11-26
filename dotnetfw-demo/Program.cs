0a53bba60bd952fed8114054af0ec4aaddca97dd
Hiroaki Miyazaki <hbellstar@gmail.com>
2025-11-26 11:48:49 +0900

    Add apps

---
+++ dotnetfw-demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetfw_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("hello world");
+        }
+    }
+}
